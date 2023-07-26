8839e59fbdae423508cb5e24e15525b4e67c106c
Oke <oakie.lukas@gmail.com>
2023-07-26 11:26:19 +0200

    final touches to improve render

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,23 +6,23 @@ export default () => {
   return (
     <>
       <nav className="fixed bottom-0 backdrop-blur-sm bg-navbar  bg-opacity-50 max-w-lg  w-full h-16 font-sans font-light ">
-        <div className="grid h-full  grid-cols-3 mx-auto text-white-100 text-xl ">
+        <div className="grid h-full  grid-cols-3 mx-auto text-xl ">
           <button type="button">
             {" "}
             <Link to="/profile">
-              <span>Profile</span>
+              <span className="text-white-100">Profile</span>
             </Link>
           </button>
 
           <button type="button">
             <Link to="/home">
-              <span>Home</span>
+              <span className="text-white-100">Home</span>
             </Link>
           </button>
 
           <button type="button">
             <Link to="/routines">
-              <span>Routines</span>
+              <span className="text-white-100">Routines</span>
             </Link>
           </button>
         </div>
